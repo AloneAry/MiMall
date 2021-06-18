509fb83c2378a6aba67209083b1ff26a49fc82d3
将军令 <961122099@qq.com>
2021-06-18 10:28:14 +0800

    storage的封装和mockJS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import Vue from 'vue'
 import router from './router/router'
+import axios from "axios"
+import VueAxios from "vue-axios"
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// mock开关
+const mock = true
+if(mock) {
+  require('./../public/mock/api.json');
+}
+//
+axios.default.baseURL = '/api'
+axios.default.timeout = 8000;
+
+//接口错误拦截
+axios.interceptors.response.use(function(response) {
+  let res = response.data;
+  if(res.status == 0) {
+    return res.data;
+  } else if(res.status == 10) {
+    window.location.href = '/#/login';
+    return Promise.reject(res);
+  } else {
+    Message.warning(res.msg);
+    return Promise.reject(res)
+  }
+},(error) => {
+  let res = error.response;
+  Message.error(res.data.message);
+  return Promise.reject(error);
+})
+
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
